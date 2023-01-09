ec47f4198e8d35190d177b2ac089f40aad1c36af
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-01-09 10:19:48 +0100

    fix: remove calls to enableProdMode()

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,2 @@
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-    enableProdMode();
-}
-
 export { renderModule } from '@angular/platform-server';
 export { AppServerModule } from './app/app.server.module';
