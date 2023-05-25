605cfcdedf3e90df2daacf5a4cd4229a94246cf8
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-05-26 00:24:42 +0200

    fix: use scss suffix for all stylesheets

--- src/app/slides/defs/defs.component.ts
+++ src/app/slides/defs/defs.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'dmx-defs',
-    styleUrls: ['./defs.component.css'],
+    styleUrls: ['./defs.component.scss'],
     templateUrl: './defs.component.html'
 })
 export class DefsComponent {}
