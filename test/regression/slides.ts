de34580fe4f306f4217f4e63204c82333be3391d
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-05-25 17:45:19 +0200

    test: run regression tests when offline

--- test/regression/slides.ts
+++ test/regression/slides.ts
@@ -1,16 +1,49 @@
+import { env } from 'process';
 import { expect, test } from '@playwright/test';
 
 test.use({ viewport: { height: 1080, width: 1920 } });
 
 for (let slide = 1; slide < 26; slide += 1) {
     test.describe(`slide ${slide}`, () => {
-        test.beforeEach(({ page }) => page.emulateMedia({ reducedMotion: 'reduce' }));
+        let name: string;
+        let path: string;
+
+        test.beforeEach(async ({ page }) => {
+            name = `slide-${slide}-should-look-the-same-1.png`;
+            path = `./slides/${slide}`;
+
+            await page.emulateMedia({ reducedMotion: 'reduce' });
+        });
 
         test('should look the same', async ({ page }) => {
-            await page.goto(`./slides/${slide}`);
+            await page.goto(path);
 
-            await expect(page).toHaveScreenshot({
+            await expect(page).toHaveScreenshot(name, {
                 fullPage: true
+            });
+        });
+
+        test.describe('when offline', () => {
+            test.beforeEach(async ({ context, page }) => {
+                test.skip(env.IS_SMOKE_TEST !== 'true', 'This test only works with an installed Service Worker.');
+
+                await page.goto(path);
+                await page.evaluate(() => navigator.serviceWorker.ready);
+                await context.setOffline(true);
+            });
+
+            test('should look the same', async ({ browserName, page }) => {
+                if (browserName === 'chromium') {
+                    await page.goto(path);
+
+                    const registration = await page.evaluate(() => navigator.serviceWorker.getRegistration());
+
+                    expect(registration).toBeDefined();
+
+                    await expect(page).toHaveScreenshot(name, {
+                        fullPage: true
+                    });
+                }
             });
         });
     });
