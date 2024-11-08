88ef22c74f492812de4d2a8bd6481556753f8498
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-11-08 19:04:23 +0100

    build(config): remove clean:runtime task

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,15 +1,6 @@
 module.exports = {
     analyze: ['sh:analyze'],
-    build: [
-        'sh:build',
-        'sh:verify',
-        'clean:source-maps',
-        'copy:404',
-        'clean:runtime',
-        'replace:csp-production',
-        'htmlmin',
-        'replace:manifest'
-    ],
+    build: ['sh:build', 'sh:verify', 'clean:source-maps', 'copy:404', 'replace:csp-production', 'htmlmin', 'replace:manifest'],
     continuous: ['sh:continuous'],
     e2e: ['sh:e2e'],
     lint: ['sh:lint-config', 'sh:lint-src', 'sh:lint-test'],
--- config/grunt/clean.js
+++ config/grunt/clean.js
@@ -1,4 +1,3 @@
 module.exports = {
-    'runtime': ['build/demuxed-2022/browser/runtime*.js'],
     'source-maps': ['build/demuxed-2022/browser/**.map']
 };
