b593ca92689785863769a20f51554a50e5340933
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-28 23:12:42 +0100

    build(config): use npm scripts for testing tasks

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,8 +1,4 @@
 module.exports = {
     analyze: ['sh:analyze'],
-    build: ['sh:build', 'sh:verify', 'sh:rimraf-source-maps', 'copy:404', 'replace:csp-production', 'htmlmin', 'replace:manifest'],
-    continuous: ['sh:continuous'],
-    e2e: ['sh:e2e'],
-    smoke: ['sh:smoke'],
-    test: ['sh:test']
+    build: ['sh:build', 'sh:verify', 'sh:rimraf-source-maps', 'copy:404', 'replace:csp-production', 'htmlmin', 'replace:manifest']
 };
--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -7,23 +7,8 @@ module.exports = () => {
         'build': {
             cmd: 'npx ng build --base-href /demuxed-2022/ --configuration production --subresource-integrity'
         },
-        'continuous': {
-            cmd: 'npx ng test'
-        },
-        'e2e': {
-            cmd: `npx playwright install --with-deps && \
-                npx playwright test --config config/playwright/config.ts`
-        },
         'rimraf-source-maps': {
             cmd: 'rimraf build/demuxed-2022/browser/**.map'
-        },
-        'smoke': {
-            cmd: `npx playwright install --with-deps && \
-                IS_SMOKE_TEST=true npx playwright test --config config/playwright/config.ts && \
-                npx hint --telemetry=off https://chrisguttandin.github.io/demuxed-2022`
-        },
-        'test': {
-            cmd: 'npx ng test --watch false'
         },
         'verify': {
             cmd: `npx bundle-buddy build/demuxed-2022/browser/*.js.map && \
