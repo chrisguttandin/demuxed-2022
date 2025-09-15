1a2ea891ff0b3694f2cc295a5608cf79ee41c717
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-09-15 02:08:21 +0200

    fix: provide context when bootstrapping the server

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
 
 // eslint-disable-next-line import/no-default-export
 export default bootstrap;
